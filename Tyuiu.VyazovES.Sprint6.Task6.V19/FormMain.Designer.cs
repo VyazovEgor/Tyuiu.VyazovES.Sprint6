d00b191527013f0c1afae7e76e89accb3fdd645e
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-12-02 00:20:36 +0500

    1

--- Tyuiu.VyazovES.Sprint6.Task6.V19/FormMain.Designer.cs
+++ Tyuiu.VyazovES.Sprint6.Task6.V19/FormMain.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            openFileDialog1 = new OpenFileDialog();
+            toolTip1 = new ToolTip(components);
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(449, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(293, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Выбрать файл и выполнить задание";
+            toolTip1.SetToolTip(button1, "Выполняет задание");
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 76);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(354, 362);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(372, 76);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(369, 362);
+            textBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(431, 58);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(753, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "FormMain";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private ToolTip toolTip1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
     }
 }
--- Tyuiu.VyazovES.Sprint6.Task6.V19/FormMain.cs
+++ Tyuiu.VyazovES.Sprint6.Task6.V19/FormMain.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Drawing.Configuration;
+using Tyuiu.VyazovES.Sprint6.Task6.V19.Lib;
+
 namespace Tyuiu.VyazovES.Sprint6.Task6.V19
 {
     public partial class FormMain : Form
@@ -6,5 +10,24 @@ namespace Tyuiu.VyazovES.Sprint6.Task6.V19
         {
             InitializeComponent();
         }
+        string openFilePath;
+        DataService ds = new DataService();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                openFileDialog1.ShowDialog();
+                openFilePath = openFileDialog1.FileName;
+                textBox1.Text = File.ReadAllText(openFilePath);
+                textBox2.Text = ds.CollectTextFromFile(openFilePath);
+                
+            }
+            catch
+            {
+                MessageBox.Show("¬ведены неверные данные", "ќшибка", MessageBoxButtons.OK);
+            }
+
+        }
+      
     }
 }
+
